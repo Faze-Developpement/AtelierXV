adf44c2766c13541051aefec69aa6b9264cd201e
Fael HENINE <fael.henine@gmail.com>
2026-04-26 12:41:33 +0200

    build: generate GitHub Pages in project docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "github-pages" ? "/faze-developpement_1/" : "/",
+  base: mode === "github-pages" ? "/AtelierXV/" : "/",
   plugins: [react()]
 }))
