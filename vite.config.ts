4e1cf6676dcae9121f0bdac583d4953ac8232bdf
Fael HENINE <fael.henine@gmail.com>
2026-04-26 12:25:28 +0200

    resolve jekyll error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig({
-  base: "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/faze-developpement_1/" : "/",
   plugins: [react()]
-})
+}))
